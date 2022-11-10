34c39a0958f3c0fa2384147d7c28569aaaf1ae00
Gonzalo Rigobello <103467918+gonrigo18@users.noreply.github.com>
2022-11-10 14:32:23 -0300

    3.3

--- desafio-con-handlebars/src/server.js
+++ desafio-con-handlebars/src/server.js
@@ -4,21 +4,26 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+const port = 8080;
+
+const products = [];
+
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
-app.set('views', './views')
+app.set('views', 'views')
 
 app.get('/', (req, res) => {
-  res.render('stock', {
-    product_name: 'Ati readeon',
-    batch: 'pn01',
-    price: 34440,
-    category: 'placas de video',
-  })
-})
+  res.render('stock', { products });
+});
 
-const port = 8080;
+app.post('/products', (req, res) => {
+  res.render('form')
+  products.push(req.body);
+  res.redirect('/');
+});
+
+
 
 const server = app.listen(port, () => {
   console.log(`Http server listening http://localhost:${port}`);
